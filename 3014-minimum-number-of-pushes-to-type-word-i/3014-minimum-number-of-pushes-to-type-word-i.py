4c55cd5e3d80d52954b399a536287266467e3339
RAHUL KUMAR SINHA <sinha.rahul2318@gmail.com>
2026-07-30 17:23:21 +0530

    Time: 0 ms (100%), Space: 19.4 MB (16.35%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -3,5 +3,5 @@ class Solution:
         n = len(word)
         pushcnt = 0
         for i in range(n):
-            pushcnt += i // 8 + 1
+            pushcnt += (i // 8 + 1)
         return pushcnt
